96d33c34fac49fe1a5678862b3793683fb20a0f4
Torsten Werner <twerner@debian.org>
2007-06-04 16:02:36 -0600

    Fix compilation on some systems

--- ext/inotify-syscalls.h
+++ ext/inotify-syscalls.h
@@ -1,8 +1,6 @@
 #ifndef _LINUX_INOTIFY_SYSCALLS_H
 #define _LINUX_INOTIFY_SYSCALLS_H
 
-#include <sys/syscall.h>
-
 #if defined(__i386__)
 # define __NR_inotify_init	291
 # define __NR_inotify_add_watch	292
@@ -22,20 +20,5 @@
 #else
 # error "Unsupported architecture!"
 #endif
-
-static inline int inotify_init (void)
-{
-	return syscall (__NR_inotify_init);
-}
-
-static inline int inotify_add_watch (int fd, const char *name, __u32 mask)
-{
-	return syscall (__NR_inotify_add_watch, fd, name, mask);
-}
-
-static inline int inotify_rm_watch (int fd, __u32 wd)
-{
-	return syscall (__NR_inotify_rm_watch, fd, wd);
-}
 
 #endif /* _LINUX_INOTIFY_SYSCALLS_H */
--- ext/inotify.c
+++ ext/inotify.c
@@ -9,6 +9,24 @@
 #include "inotify-syscalls.h"
 #endif
 
+#include <sys/syscall.h>
+#include <unistd.h>
+
+static inline int inotify_init (void)
+{
+	return syscall (__NR_inotify_init);
+}
+
+static inline int inotify_add_watch (int fd, const char *name, __u32 mask)
+{
+	return syscall (__NR_inotify_add_watch, fd, name, mask);
+}
+
+static inline int inotify_rm_watch (int fd, __u32 wd)
+{
+	return syscall (__NR_inotify_rm_watch, fd, wd);
+}
+
 VALUE rb_cInotify;
 VALUE rb_cInotifyEvent;
 
